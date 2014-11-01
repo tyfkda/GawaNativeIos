b6721a59e0b082c5b8aa491472e22544d5c44d93
tyfkda <tyfkda@gmail.com>
2014-11-01 21:44:15 +0900

    UIWebViewをフレーム全体に配置

--- GawaNativeIos/ViewController.swift
+++ GawaNativeIos/ViewController.swift
@@ -1,11 +1,23 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate {
+  var webview: UIWebView = UIWebView()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    print("ViewController#viewDidLoad")
+
+    self.webview.frame = self.view.bounds
+    self.webview.delegate = self;
+    self.view.addSubview(self.webview)
+
+    let url = NSURL(string: "http://www.example.com/")!
+    let urlRequest = NSURLRequest(URL: url)
+    self.webview.loadRequest(urlRequest)
+  }
+
+  func webView(webView: UIWebView!, shouldStartLoadWithRequest request: NSURLRequest!, navigationType: UIWebViewNavigationType) -> Bool {
+    return true
   }
 
   override func didReceiveMemoryWarning() {
