b9455fa6430a77f39f2e8ae659624e7d4453b96f
tyfkda <tyfkda@gmail.com>
2014-11-01 21:18:15 +0900

    Initial Commit

---
+++ GawaNativeIos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GawaNativeIos
+//
+//  Created by admin on 2014/11/01.
+//  Copyright (c) 2014年 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
