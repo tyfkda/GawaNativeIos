1d6870b298e9973922beed78e5c1db6342bcb575
tyfkda <tyfkda@gmail.com>
2015-01-08 09:44:57 +0900

    タッチイベントのテスト
    普通のonClickイベントは300msのディレイが挟まれてしまい、レスポンスが悪い。
    タッチイベントの場合にはそういうことがないので、レスポンスがよい。

--- GawaNativeIos/Resources/main.js
+++ GawaNativeIos/Resources/main.js
@@ -2,4 +2,18 @@ function addTextNode(text) {
   document.body.appendChild(document.createTextNode(text));
 }
 
-addTextNode('Hello, from JS');
+(function() {
+  addTextNode('Hello, from JS');
+
+  var touch = document.getElementById('touch');
+  touch.addEventListener('touchstart', function(event) {
+    touch.style.backgroundColor = 'red';
+    event.stopPropagation();
+    location.href = 'native://touchstart';
+  });
+  touch.addEventListener('touchend', function(event) {
+    touch.style.backgroundColor = '';
+    event.stopPropagation();
+    location.href = 'native://touchend';
+  });
+})();
